f1a25a48d0aa4abb57d6dba8ba1ed7b23b30eeab
Istaranjan <istaranjandas@gmail.com>
2021-09-15 20:40:45 +0530

    added input output features

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ var btntranslate = document.querySelector('#btn-translate')
 btntranslate.addEventListener('click', clickHandler)
 
 function clickHandler(){
-    console.log("CLICKED!");
-    console.log("Input:" + txtInput.value)
+   outputDiv.innerText = txtInput.value;
 }
 
 var txtInput = document.querySelector("#txt-input");
+
+var outputDiv = document.querySelector('#output');
+
+
